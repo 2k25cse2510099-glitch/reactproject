53591110dc8592ce43af45e54672dcc6e7c1e944
ABHINAV SRIVASTAVA <2k25cse2510099@gmail.com>
2026-06-10 13:09:25 +0530

    REACTJS done

---
+++ 1React_project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>HELLO DOSTO?</h1>
+      <h2>kaisi lagi chhutiyan?</h2>
+      <h2>If you are bored toh coding krlo?</h2>
+    </div>
+  )
+}
+
+export default App;  
